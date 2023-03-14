584853116717bba6247c745651d457a54464acf0
muyexi <muyexi@gmail.com>
2023-03-14 21:39:36 +0900

    feat(LoginPage): validation

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TwitterOutlined } from '@ant-design/icons';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { Button } from '@/components/Button';
 import { Input } from '@/components/Input';
 import { User } from '@/utils/type';
 import { useRouter } from 'next/navigation';
-import { login } from '@/utils/storage';
+import { login, signup } from '@/utils/storage';
 
 export default function LoginPage() {
   const {
@@ -15,18 +15,25 @@ export default function LoginPage() {
   } = useForm<User>();
 
   const router = useRouter();
+  const [error, setError] = useState(false);
 
-  const onSubmit: SubmitHandler<User> = (user) => {
-    login(user)
+  const onLogin: SubmitHandler<User> = (user) => {
+    const success = login(user);
+    if (success) {
+      router.push('/');
+    } else {
+      setError(true);
+    }
+  };
+
+  const onSignup: SubmitHandler<User> = (user) => {
+    signup(user);
     router.push('/');
   };
 
   return (
     <div className="h-screen bg-white pt-10 md:flex md:justify-center">
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="h-fit w-60 rounded bg-slate-200 px-8 pt-6 pb-8 shadow-md"
-      >
+      <form className="h-fit w-60 rounded bg-slate-200 px-8 pt-6 pb-8 shadow-md">
         <div className="mb-5 flex place-content-center">
           <TwitterOutlined className="text-3xl text-sky-500" />
         </div>
@@ -45,10 +52,26 @@ export default function LoginPage() {
             errors={errors}
           ></Input>
         </div>
+        {error && (
+          <div className="mb-2 font-sans text-xs text-red-400">
+            Wrong user name or password.
+          </div>
+        )}
 
-        <div className="flex place-content-between mt-2.5">
-          <Button primary size="medium" label="Log in" type="submit" />
-          <Button size="small" label="Sign up" type="submit" />
+        <div className="mt-2.5 flex place-content-between">
+          <Button
+            primary
+            size="medium"
+            label="Log in"
+            data-testid="login"
+            onClick={handleSubmit(onLogin)}
+          />
+          <Button
+            size="small"
+            label="Sign up"
+            type="submit"
+            onClick={handleSubmit(onSignup)}
+          />
         </div>
       </form>
     </div>
